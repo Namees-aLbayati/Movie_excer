2e47d8a2a987834ccaadb390829108a322c271b4
Namees Albayati <nameesalbayati@fb.com>
2023-06-08 13:00:27 -0500

    adding componenet

--- src/components/count.jsx
+++ src/components/count.jsx
@@ -1,53 +1,39 @@
-import { render } from '@testing-library/react';
-import React, { Component, useState } from 'react';
+import React, { Component, useState,useEffect } from 'react';
+import List from './list';
 const Home=()=>{
-console.log('test')
-const [actor,setActor]=useState()
-const actArr=[]
-function getActor() {
-    fetch('https://switch-yam-equator.azurewebsites.net/api/actors', {
-      method: 'GET',
-      headers: {
-        'x-chmura-cors': '3d088e34-eda0-4f33-a114-b7345c5c5fa9'
-      }
-    })
-      .then(response => response.json())
-      .then(data=>{
-        const filteredActors = data.filter(
-            (element) =>
-              element.name === 'Nicolas Cage' || element.name === 'Keanu Reeves'
-          );
-          setActor(filteredActors);
-        
-      })
+const [actor,setActor]=useState([])
 
-      
-}
-getActor()
+
+useEffect(() => {
+  fetch('https://switch-yam-equator.azurewebsites.net/api/actors', {
+    method: 'GET',
+    headers: {
+      'x-chmura-cors': '3d088e34-eda0-4f33-a114-b7345c5c5fa9'
+    }
+  })
+    .then(response => response.json()).then((data) => {
+    const filteredActors = data.filter(
+      (element) =>
+        element.name === 'Nicolas Cage' || element.name === 'Keanu Reeves'
+    );
+    setActor(filteredActors);
+  });
+}, []); 
 
 
 return(
-    <h1>{actArr[0]}</h1>
-)
+  <>
+<nav className="navbar bg-body-tertiary">
+  <div className="container-fluid d-flex justify-content-center ">
+    <span className="navbar-text fs-3 text-primary  ">
+List of the Actors who've been with N.C. &K.R.    </span>
+  </div>
+</nav>
+<List actor={actor}/>
+</>
+  )
 
- /* fetchData() {
-    fetch('https://switch-yam-equator.azurewebsites.net/api/movies', {
-      method: 'GET',
-      headers: {
-        'x-chmura-cors': '3d088e34-eda0-4f33-a114-b7345c5c5fa9'
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        data.forEach(element => {
-          this.getActor(element);
-        });
-      })
-      .catch(error => {
-        // Handle any errors
-        console.error(error);
-      });
-  }*/
+
 
 
 }
---
+++ src/components/liComponents.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const LiComponents=({matchingIds})=>{
+console.log('licompnenet here',matchingIds)
+}
+
+export default LiComponents;
