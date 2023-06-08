29038c0d524b780fe64e602dfbf4dfeed9539814
Namees Albayati <nameesalbayati@fb.com>
2023-06-08 16:53:23 -0500

    ..

--- src/components/liComponents.jsx
+++ src/components/liComponents.jsx
@@ -30,25 +30,37 @@ checkValidation(actors)
 },[actors])
 
 
+
 const checkValidation = async (actors) => {
-    if (actors.length === 0) {
+    try {
+      if (actors.length === 0) {
         console.log('actors array is empty');
         return;
       }
   
-console.log(actors,'act')
-    const response=await   fetch('https://switch-yam-equator.azurewebsites.net/api/validation', {
-        method: 'POST',
-        headers: {
-"Content-Type":"application/json",
-          'x-chmura-cors': '3d088e34-eda0-4f33-a114-b7345c5c5fa9'
-        },
-        body:JSON.stringify(actors)
-      });
-
-      const showingResult=await response.json();
-      console.log(showingResult,'validate results')
-
+      console.log(actors, 'act');
+  
+      const response = await fetch(
+        'https://switch-yam-equator.azurewebsites.net/api/validation',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-chmura-cors': '3d088e34-eda0-4f33-a114-b7345c5c5fa9'
+          },
+          body: JSON.stringify(actors)
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error('API request failed');
+      }
+  
+      const showingResult = await response.json();
+      console.log(showingResult, 'validate results');
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+    }
   };
   
   
